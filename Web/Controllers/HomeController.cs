dca873310cc4a6a92f0ca375e6ea2ac12e9ff624
BenyKrik123 <BenyKrik123@gmail.com>
2017-10-20 18:21:41 +0300

    Auto stash before merge of "develop" and "origin/develop"

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Afisha.Controllers
+{
+    public class HomeController : Controllers
+    {
+        public IOptions<AppSetting> AppSettings { get; }
+        public VkApi Api { get; }
+        public UnitOfWork<ApplicationDbContext> Unit { get; }
+
+        public HomeController(IHostingEnvironment environment,
+            IHttpContextAccessor accessor,
+            IMemoryCache memoryCache,
+            IOptions<AppSetting> appSettings,
+            UnitOfWork<ApplicationDbContext> unit)
+            : base(environment, accessor, memoryCache, afishaApi)
+        {
+            Unit = unit;
+            AppSettings = appSettings;
+            Api = vkApi;
+        }
+    }
+}
--- Web/Startup.cs
+++ Web/Startup.cs
@@ -1,33 +1,131 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ATMIT.Core.Web.Repository;
+using ATMIT.Web.Utility.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Models;
+//using Models.Api.AfishaApi;
+using Models.Api.VkApi;
+//using Models.Api.VkApi.Bot;
+using Models.AppSettings;
+using Models.Extensions;
+using System.Globalization;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
-namespace Web
+namespace Afisha
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            Configuration = configuration;
+            Env = env;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            ContextFactory.ConnectionString = Configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IConfiguration Configuration { get; }
+        public IHostingEnvironment Env { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddEntityFrameworkSqlServer()
+            .AddDbContext<ApplicationDbContext>((serviceProvider, options) =>
+                options.UseSqlServer(ContextFactory.ConnectionString)
+                       .UseInternalServiceProvider(serviceProvider));
+
+            if (!Env.IsDevelopment())
+            {
+                services.Configure<MvcOptions>(options =>
+                {
+                    options.Filters.Add(new RequireHttpsAttribute());
+                });
+                services.Configure<RewriteOptions>(options =>
+                {
+                    options.AddRedirectToHttps();
+                });
+            }
+            //var test = new EventNotificator(TimeSpan.FromSeconds(5));
+            //CancellableTaskProc proc = test.StartProcLoop;
+            //var task = new CancellableTask(proc, TaskCreationOptions.LongRunning, $"BotTask");
+            //task.Start();
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddMvc()
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver();
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                });
+            services.AddMemoryCache();
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.Cookie.SameSite = SameSiteMode.None;
+            });
+            services.AddTransient<UnitOfWork<ApplicationDbContext>>();
+            services.Configure<AppSetting>(Configuration);
+            services.AddSingleton<AfishaApi>();
+            services.AddSingleton<VkApi>();
+            //services.AddSingleton<AfishaBot>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IOptions<AppSetting> _appSetting, AfishaBot _afishaBot)
         {
+
             if (env.IsDevelopment())
             {
+                //_afishaBot.Start(BotTaskType.NotificationAboutEvent);
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                app.UseBrowserLink();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
+                //app.UseExceptionHandler("/Home/Error");
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            app.UseStaticFiles();
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                SupportedCultures = new List<CultureInfo> {
+                    new CultureInfo("ru")
+                }
+            });
+            //app.UseErrorHandler();
+            app.UseSession();
+            //обновляем миграции
+            if (env.IsProduction())
+                app.UseMigrations();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}");
             });
         }
     }
