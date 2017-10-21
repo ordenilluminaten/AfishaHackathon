604247e4422d5e06a8211c7cd6a481287dd7ed3e
Sergey <yakatomoshi@gmail.com>
2017-10-21 12:34:48 +0300

    fix header

--- Web/wwwroot/js/pages/Header.js
+++ Web/wwwroot/js/pages/Header.js
@@ -4,18 +4,17 @@ class Header {
     }
 
     static openFriends() {
-        debugger;
         let userIds = [1];
-        // userIds = appRactive.get('currrentUser.friends.userIds');
-        // if(userIds != null && userIds.length > 0) {
+        userIds = appRactive.get('currrentUser.friends.userIds');
+        if(userIds != null && userIds.length > 0) {
             Request.post({
                 url: '/GetUsersEventsByIds',
                 data: {
                     ids: userIds
                 }
             }).then((friendsEvents) => {
-                appRactive.set('currrentUser.friendsEvents', friendsEvents);                                                 
+                appRactive.set('currentUser.friendsEvents', friendsEvents);                                                 
             });
-        // }     
+        }     
     }
 }
