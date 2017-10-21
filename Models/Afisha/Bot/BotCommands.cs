3beaf332af1288356a3a13dbeb2f540698a00bea
Unknown <im-mo-rt@yandex.ru>
2017-10-21 16:26:26 +0300

    Bot drop fix + bot commands extend

--- Models/Afisha/Bot/BotCommands.cs
+++ Models/Afisha/Bot/BotCommands.cs
@@ -1,5 +1,7 @@
 namespace Models.Afisha.Bot {
     public class BotCommands {
+        public const string Bot = "бот";
         public const string Start = "бот старт";
+        public const string Stop = "бот стоп";
     }
 }
