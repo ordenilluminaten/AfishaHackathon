c61ecb0d40dc42d1d34bd222806bb4214604c20f
Unknown <im-mo-rt@yandex.ru>
2017-10-22 04:18:14 +0300

    index js

--- Web/wwwroot/js/index.js
+++ Web/wwwroot/js/index.js
@@ -30,7 +30,7 @@
         acceptOffer: (_ctx, idEvent, idUser) => {
             Request.post({
                 method: Request.method.post,
-                url: '/home/setFamiliarWithBot',
+                url: '/home/acceptOffer',
                 data: {
                     idEvent,
                     idUser
